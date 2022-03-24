1dc47fc326a009dfa68a1b8624eb33d83c8af06f
ShanuKumarSK <shanukumarsk157@gmail.com>
2022-03-24 11:50:36 +0530

    nothing change

---
+++ stores.js
@@ -0,0 +1,54 @@
+const storeList = [
+	
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [86.159908, 24.043801,]
+		},
+		"properties": {
+			"name": "Sagar General Store",
+			"address": "Mission Road, Chirki, Pirtand, Giridih, Jharkhand 825108, India",
+			"phone": "8651924914"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [86.1597928, 24.0430442]
+		},
+		"properties": {
+			"name": "Molu Ka Gumti",
+			"address": "Beside Chirkidih Road, Main Road, Chirki, Pirtand, Giridih, Jharkhand 825108, India",
+			"phone": "8651924914"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [86.1601328, 24.0434215]
+		},
+		"properties": {
+			"name": "Ravi Ka Thela",
+			"address": "Beside Harladih Road, Main Road, Chirki, Pirtand, Giridih, Jharkhand 825108, India",
+			"phone": "8651924914"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [77.167222, 28.653835]
+		},
+		"properties": {
+			"name": "New",
+			"address": "Beside Harladih Road, Main Road, Chirki, Pirtand, Giridih, Jharkhand 825108, India",
+			"phone": "8651924914"
+		}
+	},
+
+
+		
+];
